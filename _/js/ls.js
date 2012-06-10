be0906fe4ee7f16c0e2886df95dc9ccf436af00c
Martin Eskdale Moen <martinmoen@gmail.com>
2012-06-10 23:30:12 +0100

    Parsers for html and md files data-type define parsers very early demo of operation

--- _/js/ls.js
+++ _/js/ls.js
@@ -22,31 +22,62 @@
 
 */
 
-$(document).ready(function() {
-  var conv = new Showdown.converter();
-  var article_list_file = "/articles.ls";
-
+var html_page = function(target, source) {
   $(function() {
+    var s = $(source);
+    var loc = s.attr('data-loc') || '.';
     $.ajax({
-      url: article_list_file,
-      dataType: "text",
-    }).done(function(data) {
-      var articles_container = $(document.createElement("section"));
-      
-      $.each(data.split('\n'), function() {
-        if (this.length > 0) {
-          $.ajax({
-            url: ["articles/", this.toString(), ".md"].join(''),
-            dataType: "text"
-          }).done(function(d){
-            var article = $(document.createElement("article"));
-            article.html(conv.makeHtml(d));
-            articles_container.append(article);
-          });
-        }
-      });
-      $("#main").append(articles_container);
-    })
+      url: [loc, "/", s.attr('data-source')].join(''),
+      dataType: "html",
+      cache: false
+    }).done(function(d) {
+      $(target).html(d);
+    });
   });
+};
 
+var md_items = function(target, item_pointer) {
+    var conv = new Showdown.converter();
+    $(function() {
+      $.ajax({
+        url: $(item_pointer).attr('data-source'),
+        dataType: "text",
+        cache: false
+      }).done(function(data) {
+        var articles_container = $(document.createElement("section"));
+        var loc = $(item_pointer).attr('data-loc');
+        $.each(data.split('\n'), function() {
+          if (this.length > 0) {
+            $.ajax({
+              url: [loc, "/",this.toString(), ".md"].join(''),
+              dataType: "text",
+              cache: false
+            }).done(function(d){
+              var article = $(document.createElement("article"));
+              article.html(conv.makeHtml(d));
+              articles_container.append(article);
+            });
+          }
+        });
+        $(target).html(articles_container);
+      })
+    });
+};
+
+$(document).ready(function() {
+  $('nav ol li a').each(function(i, e) {
+    var element = $(e);
+    switch(element.attr('data-type')) {
+      case "md_items":
+        element.click(function() {
+          md_items("#main", element);
+        });
+      break;
+      case "html_page":
+        element.click(function() {
+          html_page("#main", element);
+        });
+      break;
+    }
+  });
 });
