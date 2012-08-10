0f31a8ff991519fa6cabfc0603aff1ae472df591
Martin Eskdale Moen <martin@martinmoen.com>
2012-08-10 09:02:48 +0100

    Added CSS and started to disconnect internal parts

--- _/js/ls.js
+++ _/js/ls.js
@@ -5,7 +5,7 @@
 /* trigger when page is ready */
 //$(document).ready(function (){
 
-	// your functions go here
+// your functions go here
 
 //});
 
@@ -20,49 +20,63 @@
 
    });
 
-*/
+ */
 
 var html_page = function(target, source) {
   $(function() {
-    var s = $(source);
-    var loc = s.attr('data-loc') || '.';
-    $.ajax({
-      url: [loc, "/", s.attr('data-source')].join(''),
-      dataType: "html",
-      cache: false
-    }).done(function(d) {
-      $(target).html(d);
-    });
+      var s = $(source);
+      var loc = s.attr('data-loc') || '.';
+      console.log([loc, "/", s.attr('data-source')].join(''));
+      $.ajax({
+url: [loc, "/", s.attr('data-source')].join(''),
+dataType: "html",
+cache: false
+}).done(function(d) {
+  $(target).html(d);
   });
+      });
 };
 
-var md_items = function(target, item_pointer) {
-    var conv = new Showdown.converter();
-    $(function() {
-      $.ajax({
-        url: $(item_pointer).attr('data-source'),
+var md_items = function(target, mditems_loc, mditems_list) {
+  var conv = new Showdown.converter();
+  var articles_container = $(document.createElement("section"));
+  $.each(mditems_list, function() {
+    if (this.length > 0) {
+     console.log([mditems_loc, "/",this.toString(), ".md"].join('')); 
+     $.ajax({
+        url: [mditems_loc, "/",this.toString(), ".md"].join(''),
         dataType: "text",
-        cache: false
-      }).done(function(data) {
-        var articles_container = $(document.createElement("section"));
-        var loc = $(item_pointer).attr('data-loc');
-        $.each(data.split('\n'), function() {
-          if (this.length > 0) {
-            $.ajax({
-              url: [loc, "/",this.toString(), ".md"].join(''),
-              dataType: "text",
-              cache: false
-            }).done(function(d){
-              var article = $(document.createElement("article"));
-              article.html(conv.makeHtml(d));
-              articles_container.append(article);
-            });
-          }
-        });
-        $(target).html(articles_container);
-      })
+        cache: false 
+      }).done(function(d){
+        var article = $(document.createElement("article"));
+        article.html(conv.makeHtml(d));
+        articles_container.append(article);
+      }).fail(function(jq, ts) {
+        console.log(ts);
+      });
+    }
+  });
+  $(target).html(articles_container);
+}; 
+
+
+var get_mditems = function(list_file) {
+  var list = [];
+  $.ajax({
+    url: list_file,
+    dataType: "text",
+    cache: false,
+    async: false
+  }).done(function(d) {
+    $.each(d.split('\n'), function(i, val) {
+      list.push(val);
     });
+  }).fail(function(jq, ts) {
+    console.log(ts);
+  });
+  return list;
 };
+
 
 $(document).ready(function() {
   $('nav ol li a').each(function(i, e) {
@@ -70,7 +84,7 @@ $(document).ready(function() {
     switch(element.attr('data-type')) {
       case "md_items":
         element.click(function() {
-          md_items("#main", element);
+          md_items($("#main"), element.attr('data-loc'), get_mditems(element.attr('data-source')));
         });
       break;
       case "html_page":
