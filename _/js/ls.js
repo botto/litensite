e63c8cf1bc3f096ade50bda0599274be638c8d43
Martin Eskdale Moen <martin@martinmoen.com>
2012-10-06 20:37:03 +0100

    Moved files, more general nav items. Minor cleanup

--- _/js/ls.js
+++ _/js/ls.js
@@ -1,99 +1,124 @@
 // remap jQuery to $
-(function($){})(window.jQuery);
+(function($) {
+  var html_page = function(target, source) {
+    $(function() {
+      var s = $(source);
+      var loc = s.attr('data-loc') || '.';
+      $.ajax({
+        url: [loc, "/", s.attr('data-source')].join(''),
+        dataType: "html",
+        cache: false
+      }).done(function(d) {
+        $(target).html(d);
+      });
+    });
+  };
 
-
-/* trigger when page is ready */
-//$(document).ready(function (){
-
-// your functions go here
-
-//});
-
-
-/* optional triggers
-
-   $(window).load(function() {
-
-   });
-
-   $(window).resize(function() {
-
-   });
-
- */
-
-var html_page = function(target, source) {
-  $(function() {
-    var s = $(source);
-    var loc = s.attr('data-loc') || '.';
-    console.log([loc, "/", s.attr('data-source')].join(''));
+  var show_md_item = function (target, md_item_loc) {
     $.ajax({
-      url: [loc, "/", s.attr('data-source')].join(''),
-      dataType: "html",
+      url: md_item_loc,
+      dataType: 'text',
       cache: false
     }).done(function(d) {
-      $(target).html(d);
+      var conv = new Showdown.converter();
+      $(target).html(conv.makeHtml(d))
     });
-  });
-};
-
-var md_items = function(target, mditems_loc, mditems_list) {
-  var conv = new Showdown.converter();
-  var articles_container = $(document.createElement("section"));
-  $.each(mditems_list, function() {
-    if (this.length > 0) {
-     console.log([mditems_loc, "/",this.toString(), ".md"].join('')); 
-     $.ajax({
-        url: [mditems_loc, "/",this.toString(), ".md"].join(''),
-        dataType: "text",
-        cache: false 
-      }).done(function(d){
-        var article = $(document.createElement("article"));
-        article.html(conv.makeHtml(d));
-        articles_container.append(article);
-      }).fail(function(jq, ts) {
-        console.log(ts);
-      });
-    }
-  });
-  $(target).html(articles_container);
-}; 
-
-
-var get_mditems = function(list_file) {
-  var list = [];
-  $.ajax({
-    url: list_file,
-    dataType: "text",
-    cache: false,
-    async: false
-  }).done(function(d) {
-    $.each(d.split('\n'), function(i, val) {
-      list.push(val);
-    });
-  }).fail(function(jq, ts) {
-    console.log(ts);
-  });
-  return list;
-};
-
-
-$(document).ready(function() {
-  var nav_links = $('nav ol li a');
-  nav_links.each(function(i, e) {
-    var element = $(e);
-    element.click(function() {
-      nav_links.removeClass('active');
-      element.addClass('active');
-      switch(element.attr('data-type')) {
-        case "md_items":
-          md_items($("#main"), element.attr('data-loc'), get_mditems(element.attr('data-source')));
-        break;
-        case "html_page":
-          html_page("#main", element);
-        break;
+  };
+  
+  var md_items = function(target, mditems_loc, mditems_list) {
+    var conv = new Showdown.converter();
+    var articles_container = $(document.createElement("section"));
+    $.each(mditems_list, function() {
+      if (this.length > 0) {
+       $.ajax({
+          url: [mditems_loc, "/",this.toString(), ".md"].join(''),
+          dataType: "text",
+          cache: false 
+        }).done(function(d){
+          var article = $(document.createElement("article"));
+          article.html(conv.makeHtml(d));
+          articles_container.append(article);
+        }).fail(function(jq, ts) {
+          console.log(ts);
+        });
       }
     });
+    $(target).html(articles_container);
+  }; 
+  
+  
+  var get_md_items = function(list_file) {
+    var items = [];
+    $.ajax({
+      url: list_file,
+      dataType: "text",
+      cache: false,
+      async: false
+    }).done(function(d) {
+      $.each(d.split('\n'), function(i, val) {
+        items.push(val);
+      });
+    }).fail(function(jq, ts) {
+      console.log(ts);
+    });
+    return items;
+  };
+
+  var show_pre_md_items = function(out_ele, data_loc, md_items_list) {
+    var conv = new Showdown.converter();
+    var md_items_container = $(document.createElement('section'));
+    md_items_container.addClass('md-items');
+    md_items_container.addClass(data_loc);
+    $.each(md_items_list, function() {
+      if (this.length > 0) {
+       $.ajax({
+          url: [data_loc, "/",this.toString(), "_pre.md"].join(''),
+          dataType: "text",
+          cache: false 
+        }).done(function(d){
+          var md_item = $(document.createElement("article"));
+          md_item.html(conv.makeHtml(d));
+          md_items_container.append(md_item);
+        }).fail(function(jq, ts) {
+          console.log(ts);
+        });
+      }
+    });
+    $(out_ele).html(md_items_container);
+  };
+
+  function md_item(jqcaller) {
+    console.log(jqcaller);
+  }
+
+  $(function() {
+    var nav_links = $('nav ol li a');
+    nav_links.each(function(i, e) {
+      var element = $(e);
+      var main = $('#main');
+      
+      //main.filter('.visible').bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () {
+      
+      element.click(function() {
+        nav_links.removeClass('active');
+        element.addClass('active');
+        if (typeof(window[element.attr('data-type')] == 'function')) {
+          window[element.attr('data-type')](element);
+        }
+        /*switch(element.attr('data-type')) {
+          case 'md_item':
+            show_md_item(main, element.attr('data-source'));
+          break;
+          case "md_items":
+            show_pre_md_items(main, element.attr('data-loc'), get_md_items(element.attr('data-source')))
+          break;
+          case "html_page":
+            html_page("#main", element);
+          break;
+        }*/
+      });
+    });
+    nav_links.first().click();
   });
-  nav_links.first().click();
-});
+
+})(window.jQuery);
---
+++ pages/me.md
@@ -0,0 +1,5 @@
+Currently only a PDF of my CV is available:
+[Martin Eskdale Moen] [cv]
+
+
+[cv]: /martineskdalemoen.pdf
