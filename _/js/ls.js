5847288c71563efa30e7d8194a8714053d76f3f2
Martin Eskdale Moen <martinmoen@gmail.com>
2012-11-04 16:01:16 +0000

    Complete rewrite of the handler Uses handler index name to find dom items, mainly improves performance The rendering layer and the file retrival layer are loosely decoupled, still some interdependancy. Further notes on usage will come

--- _/js/ls.js
+++ _/js/ls.js
@@ -1,124 +1,67 @@
-// remap jQuery to $
 (function($) {
-  var html_page = function(target, source) {
-    $(function() {
-      var s = $(source);
-      var loc = s.attr('data-loc') || '.';
-      $.ajax({
-        url: [loc, "/", s.attr('data-source')].join(''),
-        dataType: "html",
-        cache: false
-      }).done(function(d) {
-        $(target).html(d);
-      });
-    });
-  };
-
-  var show_md_item = function (target, md_item_loc) {
-    $.ajax({
-      url: md_item_loc,
-      dataType: 'text',
-      cache: false
-    }).done(function(d) {
-      var conv = new Showdown.converter();
-      $(target).html(conv.makeHtml(d))
-    });
-  };
-  
-  var md_items = function(target, mditems_loc, mditems_list) {
-    var conv = new Showdown.converter();
-    var articles_container = $(document.createElement("section"));
-    $.each(mditems_list, function() {
-      if (this.length > 0) {
-       $.ajax({
-          url: [mditems_loc, "/",this.toString(), ".md"].join(''),
-          dataType: "text",
-          cache: false 
-        }).done(function(d){
-          var article = $(document.createElement("article"));
-          article.html(conv.makeHtml(d));
-          articles_container.append(article);
-        }).fail(function(jq, ts) {
-          console.log(ts);
-        });
-      }
-    });
-    $(target).html(articles_container);
-  }; 
-  
-  
-  var get_md_items = function(list_file) {
-    var items = [];
-    $.ajax({
-      url: list_file,
-      dataType: "text",
-      cache: false,
-      async: false
-    }).done(function(d) {
-      $.each(d.split('\n'), function(i, val) {
-        items.push(val);
-      });
-    }).fail(function(jq, ts) {
-      console.log(ts);
-    });
-    return items;
-  };
-
-  var show_pre_md_items = function(out_ele, data_loc, md_items_list) {
-    var conv = new Showdown.converter();
-    var md_items_container = $(document.createElement('section'));
-    md_items_container.addClass('md-items');
-    md_items_container.addClass(data_loc);
-    $.each(md_items_list, function() {
-      if (this.length > 0) {
-       $.ajax({
-          url: [data_loc, "/",this.toString(), "_pre.md"].join(''),
-          dataType: "text",
-          cache: false 
-        }).done(function(d){
-          var md_item = $(document.createElement("article"));
-          md_item.html(conv.makeHtml(d));
-          md_items_container.append(md_item);
-        }).fail(function(jq, ts) {
-          console.log(ts);
-        });
-      }
-    });
-    $(out_ele).html(md_items_container);
-  };
-
-  function md_item(jqcaller) {
-    console.log(jqcaller);
-  }
-
-  $(function() {
-    var nav_links = $('nav ol li a');
-    nav_links.each(function(i, e) {
-      var element = $(e);
-      var main = $('#main');
-      
-      //main.filter('.visible').bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () {
-      
-      element.click(function() {
-        nav_links.removeClass('active');
-        element.addClass('active');
-        if (typeof(window[element.attr('data-type')] == 'function')) {
-          window[element.attr('data-type')](element);
+  var theme = (function(id) {
+    var formatting_engines = {};
+    var formatters = {
+      'md_item': function (markdown) {
+        //Check if we have instantiated the themeing engine before
+        if (typeof formatting_engines['markdown'] != 'function') {
+          formatting_engines['markdown'] = new Showdown.converter();
         }
-        /*switch(element.attr('data-type')) {
-          case 'md_item':
-            show_md_item(main, element.attr('data-source'));
-          break;
-          case "md_items":
-            show_pre_md_items(main, element.attr('data-loc'), get_md_items(element.attr('data-source')))
-          break;
-          case "html_page":
-            html_page("#main", element);
-          break;
-        }*/
+        return formatting_engines['markdown'].makeHtml(markdown);
+      },
+    }
+    if (typeof id == 'undefined' || id.length <= 0) {
+      return formatters;
+    }
+    else {
+      return (formatters[id] ? formatters[id] : false);
+    }
+  });
+  
+  var handler = (function(id) {
+    var handlers = {
+      'md_item': function(attr) {
+        $.ajax(attr['source'])
+          .done(function(md) {
+            $(attr['target']).html(theme('md_item')(md));
+          });
+      },
+      'md_items': function(attr) {
+        $.ajax(attr['source'])
+          .done(function(mdmani) {
+            //Reuild the location of the index file, but only if ajax was sucesfull
+            var md_files_loc = attr['source'].split('/');
+            md_files_loc.pop();
+            md_files_loc = md_files_loc.join(' ');
+            $.each(mdmani.split('\n'), function (i, e) {
+              if (e.length > 0) {
+                handler('md_item')({
+                  'source': [md_files_loc, '/', e, '.md'].join(''),
+                  'target': attr['target']
+                });
+              }
+            });
+          });
+      }
+    }
+    if (typeof(id) == 'undefined' || id.length <= 0) {
+      return handlers;
+    }
+    else {
+      return (handlers[id] ? handlers[id] : false);
+    }
+  });
+  
+  $(function() {
+    $.each(handler(), function(i,e) {
+      $(['[data-type="',i,'"]'].join('')).click(function() {
+        var e = $(this);
+        handler(i)({
+          'target': e.attr('data-target'),
+          'source': e.attr('data-source') 
+        });
       });
     });
-    nav_links.first().click();
+    $('nav ol li a').first().click();
   });
-
 })(window.jQuery);
