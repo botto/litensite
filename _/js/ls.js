cf7c636f49ee891cde008a2cabbe66a446ff70fc
Martin Eskdale Moen <martinmoen@gmail.com>
2012-11-07 09:18:23 +0000

    Updated to jquery 182 and backports from mmcom

--- _/js/ls.js
+++ _/js/ls.js
@@ -1,72 +1,67 @@
-// remap jQuery to $
-(function($){})(window.jQuery);
-
-
-/* trigger when page is ready */
-//$(document).ready(function (){
-
-	// your functions go here
-
-//});
-
-
-/* optional triggers
-
-$(window).load(function() {
-	
-});
-
-$(window).resize(function() {
-	
-});
-
-*/
-
-$(document).ready(function() {
-  var conv = new Showdown.converter();
-  var page_list_file = "pages.ls";
-
-  $(function() {
-   $.ajax({
-      url: page_list_file,
-      type: "text"
-  }).done(function(data) {
-    $.each(data.split('\n'), function() {
-      if (this.length > 0) {
-        var page_link_ele = $(document.createElement("a"));
-        var page_link_ele_wrapper = $(document.createElement("li"));
-        var page_link = this.toString();
-        $(page_link_ele).html(this.toString());
-        $(page_link_ele).attr("class", "page");
-        $(page_link_ele).attr("id", page_link);
-        $(page_link_ele).attr("href", "#"+page_link);
-        $(page_link_ele).on("click", null, function() {
-          $.ajax({
-            url: ["page/",page_link].join('')
-          }).done(function(txt) {
-            $("#content").html(conv.makeHtml(txt));
-          });
-        });
-        $(page_link_ele_wrapper).append(page_link_ele);
-        $("#right-nav").append(page_link_ele_wrapper);
-      }
-    });
+(function($) {
+  var theme = (function(id) {
+    var formatting_engines = {};
+    var formatters = {
+      'md_item': function (markdown) {
+        //Check if we have instantiated the themeing engine before
+        if (typeof formatting_engines['markdown'] != 'function') {
+          formatting_engines['markdown'] = new Showdown.converter();
+        }
+        return formatting_engines['markdown'].makeHtml(markdown);
+      },
+    }
+    if (typeof id == 'undefined' || id.length <= 0) {
+      return formatters;
+    }
+    else {
+      return (formatters[id] ? formatters[id] : false);
+    }
   });
   
+  var handler = (function(id) {
+    var handlers = {
+      'md_item': function(attr) {
+        $.ajax(attr['source'])
+          .done(function(md) {
+            $(attr['target']).html(theme('md_item')(md));
+          });
+      },
+      'md_items': function(attr) {
+        $.ajax(attr['source'])
+          .done(function(mdmani) {
+            //Reuild the location of the index file, but only if ajax was sucesfull
+            var md_files_loc = attr['source'].split('/');
+            md_files_loc.pop();
+            md_files_loc = md_files_loc.join(' ');
+            $.each(mdmani.split('\n'), function (i, e) {
+              if (e.length > 0) {
+                handler('md_item')({
+                  'source': [md_files_loc, '/', e, '.md'].join(''),
+                  'target': attr['target']
+                });
+              }
+            });
+          });
+      }
+    }
+    if (typeof(id) == 'undefined' || id.length <= 0) {
+      return handlers;
+    }
+    else {
+      return (handlers[id] ? handlers[id] : false);
+    }
   });
-
+  
   $(function() {
-    $(".nav a").each(function(i, ele) {
-      var item = $(ele);
-      item.on("click", null ,function () {
-        $.ajax({
-          url: [item.attr("class"),"/",item.attr("id")].join(''),
-          type: "text"
-        }).done(function(txt) {
-          $("#content").html(conv.makeHtml(txt));
+    $.each(handler(), function(i,e) {
+      $(['[data-type="',i,'"]'].join('')).click(function() {
+        var e = $(this);
+        handler(i)({
+          'target': e.attr('data-target'),
+          'source': e.attr('data-source') 
         });
       });
     });
+    $('nav ol li a').first().click();
   });
-
-});
+})(window.jQuery);
